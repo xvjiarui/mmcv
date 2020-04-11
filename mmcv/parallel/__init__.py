03acf0a012a428ce1cb4db810a1a1ca098ac2cce
xurui4 <xurui4@sensetime.com>
2020-04-11 11:34:59 +0800

    merge pat support

--- mmcv/parallel/__init__.py
+++ mmcv/parallel/__init__.py
@@ -2,7 +2,13 @@
 from .collate import collate
 from .data_container import DataContainer
 from .data_parallel import MMDataParallel
-from .distributed import MMDistributedDataParallel
+try:
+    from .distributed import MMDistributedDataParallel
+except:
+    from .distributed_deprecated import MMDistributedDataParallel
+    import warnings
+    warnings.warn('Cannot import official DistributedDataParallel and use a'
+                  ' deprecated version instead.')
 from .scatter_gather import scatter, scatter_kwargs
 
 __all__ = [
