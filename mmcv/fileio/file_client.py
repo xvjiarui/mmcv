fef28afa9c33e8b41b24ff187a4feae69de268df
Jiarui XU <xvjiarui0826@gmail.com>
2020-05-22 13:04:03 +0800

    fixed import

--- mmcv/fileio/file_client.py
+++ mmcv/fileio/file_client.py
@@ -64,12 +64,12 @@ class PetrelBackend(BaseStorageBackend):
 
     def __init__(self, path_mapping=None):
         try:
-            import petrel_client
+            from petrel_client.client import Client
         except ImportError:
             raise ImportError('Please install petrel_client to enable '
                               'PetrelBackend.')
 
-        self._client = petrel_client.client.Client()
+        self._client = Client()
         assert isinstance(path_mapping, dict) or path_mapping is None
         self.path_mapping = path_mapping
 
